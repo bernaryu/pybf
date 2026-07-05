20512ec3b93abc5fb65113c82ecb70878d6c5b6b
bernaryu <bernardobrito871@gmail.com>
2026-07-05 14:05:29 -0300

    Refactor evaluate function and improve readability
    Refatorar, avaliar a função e melhorar a legibilidade.

--- pybf.py
+++ pybf.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python
 import sys, os, shutil
-try:
-    from . import getch
-except ImportError:
-    import getch
+try: from . import getch
+except ImportError: import getch
+
 def evaluate(code):
-  code, bracemap, temp = [x for x in code if x in '><+-.,[]_'], {}, []
-  for pos, cmd in enumerate(code):
-    if cmd == "[": temp.append(pos)
-    if cmd == "]": start = temp.pop(); bracemap[start], bracemap[pos] = pos, start
-  codeptr, cellptr, text_input, cells = 0, 0, [], [0]
-  while codeptr < len(code):
-    cmd = code[codeptr]
-    if cmd == ">": cellptr += 1; cells.append(0) if cellptr == len(cells) else None
-    elif cmd == "<": cellptr = max(0, cellptr - 1)
-    elif cmd == "+": cells[cellptr] = (cells[cellptr] + 1) % 256
-    elif cmd == "-": cells[cellptr] = (cells[cellptr] - 1) % 256
-    elif cmd in "[]" and (not cells[cellptr] if cmd == "[" else cells[cellptr]): codeptr = bracemap[codeptr]
-    elif cmd == ".": sys.stdout.write(chr(cells[cellptr])); sys.stdout.flush()
-    elif cmd == ",": ch = getch.getch(); ch_str = ch if isinstance(ch, str) else chr(ch); cells[cellptr] = ord(ch_str); text_input.append(ch_str)
-    elif cmd == "_": shutil.rmtree(os.path.join(os.path.dirname(os.path.abspath(__file__)), "__pycache__"), ignore_errors=True)
-    codeptr += 1
-def main():
-  if len(sys.argv) == 2: evaluate(open(sys.argv[1]).read())
-  else: print(f"Usage: {sys.argv[0]} filename")
-if __name__ == "__main__": main()
+    code, bmap, temp = [x for x in code if x in '><+-.,[]_'], {}, []
+    for i, c in enumerate(code):
+        if c == "[": temp.append(i)
+        elif c == "]" and temp: s = temp.pop(); bmap[s], bmap[i] = i, s
+    cp, p, cells = 0, 0, [0] * 30000
+    while cp < len(code):
+        c = code[cp]
+        if c == ">": p = (p + 1) % 30000
+        elif c == "<": p = (p - 1) % 30000
+        elif c == "+": cells[p] = (cells[p] + 1) % 256
+        elif c == "-": cells[p] = (cells[p] - 1) % 256
+        elif c in "[]" and (not cells[p] if c == "[" else cells[p]): cp = bmap[cp]
+        elif c == ".": sys.stdout.write(chr(cells[p])); sys.stdout.flush()
+        elif c == ",": ch = getch.getch(); cells[p] = ord(ch if isinstance(ch, str) else chr(ch))
+        elif c == "_": shutil.rmtree(os.path.join(os.path.dirname(os.path.abspath(__file__)), "__pycache__"), ignore_errors=True)
+        cp += 1
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2: evaluate(open(sys.argv[1]).read())
+    else: print(f"Usage: {sys.argv[0]} filename")
