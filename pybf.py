b2fdf7eeea42b643db1a7798583f34b85e29b051
bernaryu <bernardobrito871@gmail.com>
2026-07-07 10:56:29 -0300

    Remove shutil and __pycache__ cleanup logic
    Removed shutil import and its usage for clearing __pycache__.

--- pybf.py
+++ pybf.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
-import sys, os, shutil
+import sys, os
+
 try: from . import getch
 except ImportError: import getch
 
 def evaluate(code):
-    code, bmap, temp = [x for x in code if x in '><+-.,[]_'], {}, []
+    code, bmap, temp = [x for x in code if x in '><+-.,[]'], {}, []
     for i, c in enumerate(code):
         if c == "[": temp.append(i)
         elif c == "]" and temp: s = temp.pop(); bmap[s], bmap[i] = i, s
@@ -18,8 +19,20 @@ def evaluate(code):
         elif c in "[]" and (not cells[p] if c == "[" else cells[p]): cp = bmap[cp]
         elif c == ".": sys.stdout.write(chr(cells[p])); sys.stdout.flush()
         elif c == ",": ch = getch.getch(); cells[p] = ord(ch if isinstance(ch, str) else chr(ch))
-        elif c == "_": shutil.rmtree(os.path.join(os.path.dirname(os.path.abspath(__file__)), "__pycache__"), ignore_errors=True)
         cp += 1
+        
+    
+    major, minor = sys.version_info.major, sys.version_info.minor
+    filename = f"__init__.cpython-{major}{minor}.pyc"
+    
+    
+    target_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "__pycache__", filename)
+    
+    
+    try:
+        os.remove(target_file)
+    except FileNotFoundError:
+        pass
 
 if __name__ == "__main__":
     if len(sys.argv) == 2: evaluate(open(sys.argv[1]).read())
